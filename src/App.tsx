da2aa81a73fc70a29cdcc0749dc0352ae9df448f
NeilK15 <neiltkapur@gmail.com>
2023-06-23 00:34:50 -0400

    Finished intro tutorial

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,23 @@
-import ListGroup from './components/ListGroup';
+import { useState } from 'react';
+import Alert from './components/Alert';
+import Button from './components/Button';
 
 function App() {
-  const items = ['New York', 'San Francisco', 'Tokyo', 'London', 'Paris'];
-
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
+  const [isAlerting, setIsAlerting] = useState(false);
 
   return (
     <div>
-      <ListGroup
-        items={items}
-        heading="Cities"
-        onSelectItem={handleSelectItem}
-      />
+      {isAlerting && (
+        <Alert onClose={() => setIsAlerting(false)}>My Alert</Alert>
+      )}
+      <Button
+        color="primary"
+        onClick={() => {
+          setIsAlerting(true);
+        }}
+      >
+        My Button
+      </Button>
     </div>
   );
 }
