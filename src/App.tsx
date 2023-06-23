1440fee78597100d7e2bd574bcc0d93038b0bbb6
NeilK15 <neiltkapur@gmail.com>
2023-06-22 23:34:39 -0400

    Added ListGroup component using bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
-import Message from "./Message";
+import ListGroup from './components/ListGroup';
 
 function App() {
+  const items = ['New York', 'San Francisco', 'Tokyo', 'London', 'Paris'];
+
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
+
   return (
     <div>
-      <Message />
+      <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectItem={handleSelectItem}
+      />
     </div>
   );
 }
